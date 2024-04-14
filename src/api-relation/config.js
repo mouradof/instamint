2754b7511574e528e9e2f2c22d9364964d2a2fe1
Loic Chaufournais <chaufournais.loic@gmail.com>
2024-04-14 16:54:17 +0200

    setup for sequelize and modify our last config

---
+++ src/api-relation/config.js
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv"
+import { resolve } from "node:path"
+import { Sequelize } from "sequelize"
+import sequelizeConfig from "./sequelizeConfig"
+
+dotenv.config({ path: resolve(".env.local") })
+const sequelize = new Sequelize(sequelizeConfig.development)
+
+const config = {
+  port: 3000,
+  db: sequelize
+}
+
+export default config
