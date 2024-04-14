cb377d9b65b491c88c5b9a70fe08f7fab64fa749
Loic Chaufournais <chaufournais.loic@gmail.com>
2024-04-14 22:39:34 +0200

    Create my first seed for teabag

---
+++ src/api-relation/src/db/seeds/01_initalTeabags.js
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker"
+
+export const seed = async (knex) => {
+  await knex.raw("TRUNCATE TABLE teabags RESTART IDENTITY CASCADE")
+  const teabagsData = []
+  const numTeabags = 10 
+
+  for (let i = 0; i < numTeabags; i++) {
+    teabagsData.push({
+      name: faker.commerce.productName(),
+      description: faker.lorem.sentence(),
+      createDate: faker.date.past(),
+      updatedDate: faker.date.recent(),
+      private: faker.datatype.boolean(), // Utilisation de faker.datatype.boolean() pour générer une valeur booléenne aléatoire
+    })
+  }
+
+  await knex("teabags").insert(teabagsData)
+}
