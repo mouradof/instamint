16f341d638313d11c5a4423f24f04f0516bac166
Loic Chaufournais <chaufournais.loic@gmail.com>
2024-04-14 22:29:37 +0200

    Rework setup to do js instead ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-import type { Config } from "tailwindcss"
-
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,6 +14,6 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
